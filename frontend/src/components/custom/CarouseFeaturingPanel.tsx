c9cbec0e3ebd3406356f74f2970763e20f1a6c3c
hoang.pham <hoang.pham@executionlab.asia>
2025-01-05 10:01:11 +0700

    chore

--- frontend/src/components/custom/CarouseFeaturingPanel.tsx
+++ frontend/src/components/custom/CarouseFeaturingPanel.tsx
@@ -10,6 +10,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import { Card, CardContent } from "../ui/card";
+import { Skeleton } from "@/components/ui/skeleton"; // Import the Skeleton component
 
 import Image from "next/image";
 import { useFeaturingRepo } from "@/hooks/user-landingRepo";
@@ -44,17 +45,17 @@ const CarouselSlide = ({
           className="object-cover"
         />
         {/* Floating Card */}
-        <Card className="absolute top-[15%] left-[10%] p-2 m-0 rounded-none max-w-96 bg-card  shadow-lg">
+        <Card className="absolute top-[15%] left-[10%] p-2 m-0 rounded-none max-w-96 bg-card shadow-lg">
           <CardContent className="items-center justify-center p-4">
             <h1 className="text-xl font-bold">{heading}</h1>
-            <p className="text-sm text-muted-foreground ">{text}</p>
+            <p className="text-sm text-muted-foreground">{text}</p>
           </CardContent>
         </Card>
       </CardContent>
     </Card>
 
     <div className="block sm:hidden">
-      <Card className=" p-0 m-0 rounded-none">
+      <Card className="p-0 m-0 rounded-none">
         <CardContent className="flex items-center justify-center p-0 m-0">
           <Image
             src={mobileMedia}
@@ -68,15 +69,26 @@ const CarouselSlide = ({
       </Card>
       <div className="p-4">
         <h1 className="text-xl font-bold">{heading}</h1>
-        <p className="text-sm text-muted-foreground ">{text}</p>
+        <p className="text-sm text-muted-foreground">{text}</p>
       </div>
     </div>
+  </CarouselItem>
+);
+
+const CarouselSkeleton = () => (
+  <CarouselItem>
+    <Card className="p-0 m-0 hidden sm:block rounded-none">
+      <CardContent className="relative items-center justify-center p-0 m-0">
+        <Skeleton className="w-full h-[500px] sm:h-[380px] object-cover animate-pulse" />
+      </CardContent>
+    </Card>
   </CarouselItem>
 );
 
 export function CarouseFeaturingPanel() {
   const featuringRepo = useFeaturingRepo();
   const [slides, setSlides] = useState<Featuring[]>([]);
+  const [loading, setLoading] = useState(true); // Loading state
   const plugin = React.useRef(
     Autoplay({ delay: 4000, stopOnInteraction: true })
   );
@@ -87,24 +99,29 @@ export function CarouseFeaturingPanel() {
       if (serverResponse) {
         setSlides(serverResponse.data);
       }
+      setLoading(false); // Set loading to false once data is fetched
     };
     fetchFeaturingFeature();
   }, [featuringRepo]);
 
   return (
-    <div className="relative w-full ">
+    <div className="relative w-full">
       <Carousel className="w-full" plugins={[plugin.current]}>
         <CarouselContent>
-          {slides.map((slide, index) => (
-            <CarouselSlide key={index} {...slide} />
-          ))}
+          {loading
+            ? Array.from({ length: 3 }).map((_, index) => (
+                <CarouselSkeleton key={index} />
+              ))
+            : slides.map((slide, index) => (
+                <CarouselSlide key={index} {...slide} />
+              ))}
         </CarouselContent>
         <CarouselPrevious
           className="absolute top-1/2 left-[40px] transform -translate-y-1/2 z-10 bg-card bg-opacity-50 p-2 rounded-full shadow-md hover:bg-opacity-75"
           aria-label="Previous slide"
         />
         <CarouselNext
-          className="absolute top-1/2 right-[40px] transform -translate-y-1/2 z-10 bg-card  bg-opacity-50 p-2 rounded-full shadow-md hover:bg-opacity-75"
+          className="absolute top-1/2 right-[40px] transform -translate-y-1/2 z-10 bg-card bg-opacity-50 p-2 rounded-full shadow-md hover:bg-opacity-75"
           aria-label="Next slide"
         />
       </Carousel>
--- frontend/src/components/custom/UserArchivesPanel.tsx
+++ frontend/src/components/custom/UserArchivesPanel.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import {
@@ -8,38 +10,55 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Image from "next/image";
+import Autoplay from "embla-carousel-autoplay";
 
-function UserArchivesPanel() {
-  const testimonials = [
-    {
-      id: 1,
-      text: "Udemy was rated the most popular online course or certification program for learning how to code according to StackOverflow's 2023 Developer survey.",
-      logo: "https://upload.wikimedia.org/wikipedia/commons/0/02/Stack_Overflow_logo.svg",
-      link: "View Web Development courses",
-    },
-    {
-      id: 2,
-      text: "Udemy was truly a game-changer and a great guide for me as we brought Dimensional to life.",
-      author: "Alvin Lim",
-      role: "Technical Co-Founder, CTO at Dimensional",
-      link: "View this iOS & Swift course",
-    },
-    {
-      id: 3,
-      text: "Udemy gives you the ability to be persistent. I learned exactly what I needed to know in the real world. It helped me sell myself to get a new role.",
-      author: "William A. Wachlin",
-      role: "Partner Account Manager at Amazon Web Services",
-      link: "View this AWS course",
-    },
-    {
-      id: 4,
-      text: "With Udemy Business employees were able to marry the two together, technology and consultant soft skills... to help drive their careers forward.",
-      author: "Ian Stevens",
-      role: "Head of Capability Development, North America at Publicis Sapient",
-      link: "Read full story",
-    },
-  ];
+interface Testimonial {
+  id: number;
+  text: string;
+  logo?: string; // Optional, only some testimonials have a logo
+  description?: string; // Optional, only for logo-based testimonials
+  author?: string; // Optional, only for testimonials without logos
+  role?: string; // Optional, only for testimonials without logos
+  link: string;
+}
+
+const testimonials: Testimonial[] = [
+  {
+    id: 1,
+    text: "Udemy was rated the most popular online course or certification program for learning how to code according to StackOverflow's 2023 Developer survey.",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/0/02/Stack_Overflow_logo.svg",
+    description: "37,076 responses collected",
+    link: "View Web Development courses",
+  },
+  {
+    id: 2,
+    text: "Udemy was truly a game-changer and a great guide for me as we brought Dimensional to life.",
+    author: "Alvin Lim",
+    role: "Technical Co-Founder, CTO at Dimensional",
+    link: "View this iOS & Swift course",
+  },
+  {
+    id: 3,
+    text: "Udemy gives you the ability to be persistent. I learned exactly what I needed to know in the real world. It helped me sell myself to get a new role.",
+    author: "William A. Wachlin",
+    role: "Partner Account Manager at Amazon Web Services",
+    link: "View this AWS course",
+  },
+  {
+    id: 4,
+    text: "With Udemy Business employees were able to marry the two together, technology and consultant soft skills... to help drive their careers forward.",
+    author: "Ian Stevens",
+    role: "Head of Capability Development, North America at Publicis Sapient",
+    link: "Read full story",
+  },
+];
+
+const UserArchivesPanel: React.FC = () => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 4000, stopOnInteraction: true })
+  );
 
   return (
     <div className="w-full flex flex-col">
@@ -51,6 +70,7 @@ function UserArchivesPanel() {
         opts={{
           align: "start",
         }}
+        plugins={[plugin.current]}
         orientation="horizontal"
         className="w-full"
       >
@@ -58,34 +78,62 @@ function UserArchivesPanel() {
           {testimonials.map((testimonial) => (
             <CarouselItem
               key={testimonial.id}
-              className="pt-1 sm:basis-1/2 md:basis-1/3 lg:basis-1/4"
+              className="pt-1 sm:basis-1/2 md:basis-1/3 lg:basis-1/4 min-w-[300px]"
             >
               <div className="p-1">
-                <Card className=" h-[250px]">
-                  <CardContent className="p-4">
+                <Card className="h-[250px]">
+                  <CardContent className="p-4 flex flex-col gap-4 items-start justify-center">
+                    <Image
+                      src="/quote.svg"
+                      alt="quote icon"
+                      width={17}
+                      height={17}
+                    />
+
+                    <p className="text-sm h-[80px]">{testimonial.text}</p>
+
                     {testimonial.logo && (
-                      <div className="flex justify-center mb-4">
+                      <div className="flex flex-col h-[50px] justify-center items-start gap-2">
                         <Image
                           src={testimonial.logo}
                           alt="logo"
                           layout="intrinsic" // Use intrinsic to preserve image aspect ratio
                           className="object-contain"
                           width={80}
-                          height={20} // Set height proportional to the image
+                          height={40}
                         />
+                        <p className="text-muted-foreground text-xs">
+                          {testimonial.description}
+                        </p>
                       </div>
                     )}
-                    <p className="text-sm mb-2">{testimonial.text}</p>
-                    {testimonial.author && (
-                      <p className="font-semibold text-sm">
-                        {testimonial.author}
-                      </p>
+
+                    {!testimonial.logo && (
+                      <div className="flex flex-row gap-4 justify-center items-center h-[50px]">
+                        <Avatar style={{ width: "40px", height: "40px" }}>
+                          <AvatarImage
+                            src="https://github.com/shadcn.png"
+                            alt="@shadcn"
+                            style={{ width: "40px", height: "40px" }}
+                          />
+                          <AvatarFallback
+                            style={{ fontSize: "12px", lineHeight: "40px" }}
+                          >
+                            CN
+                          </AvatarFallback>
+                        </Avatar>
+
+                        <div className="flex flex-col">
+                          <p className="text-muted-foreground text-xs">
+                            {testimonial.author}
+                          </p>
+                          <p className="text-muted-foreground text-xs">
+                            {testimonial.role}
+                          </p>
+                        </div>
+                      </div>
                     )}
-                    {testimonial.role && (
-                      <p className="text-xs text-muted-foreground500">
-                        {testimonial.role}
-                      </p>
-                    )}
+
                     <a
                       href="#"
                       className="text-blue-500 text-sm font-bold mt-2 block"
@@ -103,6 +151,6 @@ function UserArchivesPanel() {
       </Carousel>
     </div>
   );
-}
+};
 
 export default UserArchivesPanel;
